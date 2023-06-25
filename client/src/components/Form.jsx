183304432b2737ad76f43fcd1b59556f1c816bb4
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-06-25 16:24:23 +0600

    add todllist

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Form = () => {
+  return (
+    <form >
+        <input type="text" placeholder='Add new todo' />
+        <button type='submit'>Add</button>
+    </form>
+  )
+}
---
+++ client/src/components/TodoItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TodoItem = () => {
+  return (
+    <div className='todo-item'>
+      <p>this is the item</p>
+      <button>Update</button>
+      <button>Delete</button>
+    </div>
+  )
+}
